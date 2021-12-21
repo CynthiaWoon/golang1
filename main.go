9958d121a28fe034c5c1ad21b50a82617e1c3aaa
CynthiaWoon <woonstian@gmail.com>
2021-12-21 18:19:07 +0800

    non pointer values and pointer wrapper values

--- main.go
+++ main.go
@@ -229,6 +229,40 @@ func main() {
 	phonebook[456734566] = "yoshi"
 	fmt.Println(phonebook)
 
+	//group 1 types (non-pointer values) -> string, ints, bools, floats, arrays, structs
+	name := "tifa"
+
+	updateName(name) //the value of variable name above is not going to be changed inside the function
+	//everytime we pass a value or variable into a function, Go creates a copy of the variable, and then inside the function
+	//we are actually updating the copy of the variable, not the original one that we defined above before the function called
+	fmt.Println(name)
+
+	//if you really want to update the original variable
+	name = updateSecName(name) //return the value from the function and overwrite the variable value
+	fmt.Println(name)
+
+	//group 2 types (pointer wrapper values) -> maps, slices, functions
+	newMenu := map[string]float64{
+		"pie":       5.95,
+		"ice cream": 3.99,
+	}
+
+	updateMenu(newMenu) //the new element from the function is added in the map which change the original value of map
+	fmt.Println(newMenu)
+
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
+func updateName(x string) {
+	x = "wedge"
+}
+
+func updateSecName(x string) string {
+	x = "wedge"
+	return x
 }
 
 //function with return value
